071b60d16386fb329b001041fe1d6c467ca6142f
90dy <90dy@proton.me>
2025-09-25 17:28:18 +0200

    fix: cluster labels
    Signed-off-by: 90dy <90dy@proton.me>

--- internal/controller/contabomachine_controller.go
+++ internal/controller/contabomachine_controller.go
@@ -512,11 +512,7 @@ func (r *ContaboMachineReconciler) ContaboClusterToContaboMachines(ctx context.C
 		return result
 	}
 
-	clusterName, ok := cluster.Labels[clusterv1.ClusterNameLabel]
-	if !ok {
-		log.Info("ContaboCluster does not have cluster label")
-		return result
-	}
+	clusterName := cluster.ObjectMeta.Name
 
 	machineList := &clusterv1.MachineList{}
 	if err := r.List(ctx, machineList, client.InNamespace(cluster.Namespace), client.MatchingLabels{clusterv1.ClusterNameLabel: clusterName}); err != nil {
